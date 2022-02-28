95d270569a8a3eb12c60389bcfaf27d9fdfe0e15
irisktaker <ponanzar@gmail.com>
2022-02-28 11:55:03 +0300

    Add File Structure and colors

---
+++ lib/constantces.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final color = AppColors();
+  Color grey = const Color(0xFF999999);
+  Color yellow = const Color(0xFFFFCE00);
+  Color pink = const Color(0xFF009DFF);
+  Color blue = const Color(0xFF404040);
+  Color white = const Color(0xFFFFFFFF);
+  Color red = const Color(0xFFD6001B);
+  List<Color> gradientRed = [
+    const Color(0xFFD6001B),
+    const Color(0xFFFF0020),
+  ];
+}

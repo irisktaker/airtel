d2b9c04b75a5822237569d38c6c41b1389273160
irisktaker <ponanzar@gmail.com>
2022-02-28 12:11:36 +0300

    Adding Splah Screen

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../../constantces.dart';
+import '../welcome_screen/welcome_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const WelcomeScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: AppColors.color.gradientRed,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/logo.png"),
+              Image.asset("assets/images/brandname.png"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
